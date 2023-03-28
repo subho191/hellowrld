af88edfa953dd150b52e9074bdd82977371a17be
subho191 <43693753+subho191@users.noreply.github.com>
2023-03-28 18:17:57 +0530

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,6 +1,6 @@
-	#include<stdio.h>
+		#include<stdio.h>
 	#include<stdlib.h>
-	#define maxsize 10;
+	#define maxsize 10
 	void insert();
 	void delete();
 	void display();
@@ -14,7 +14,7 @@
 			printf("\n**Menu***\n");
 			printf("\n======================\n");
 			printf("\n1. Insert an element \n2.Delete an element \n3.display the queue\n4.Exit\n");
-			printf("\nEnter the choice: ")
+			printf("\nEnter the choice: ");
 			scanf("%d",&choice);
 			switch(choice)
 			{
@@ -49,26 +49,25 @@
 		queue[rear] = item;
 		printf("\nValue inserted\n");
 	}
-	void delete();
+	void delete()
 	{
-	int item;
-	if(front == -1 || front > rear)
-	{
+		int item;
+		if(front == -1 || front > rear)
+		{
 		printf("\nUnderflown\n");
 		return ;
-	}
+		}
 	else
-	item = queue[front];
-	if(front == rear)
+		item = queue[front];
+		if(front == rear)
 	{
 		front = -1;
 		rear = -1;
 	}
 	else
-	front = front+1;
-	printf("\nValue deleted %d\n",item);
+		front = front+1;
+		printf("\nValue deleted %d\n",item);
 	}
-}
 	void display()
 	{
 	int i;
@@ -78,6 +77,6 @@
 	{
 		printf("\n Printing values......\n");
 		for(i=front;i<rear;i++)
-			printf("\n5d\n",queue[i]);
+			printf("\n%d\n",queue[i]);
 		}
 	}		
